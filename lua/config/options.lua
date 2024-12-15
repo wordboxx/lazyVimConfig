4931c97e6b31d1612cda67f7e8b2d637e5c470fa
wordboxx <publicthreat@proton.me>
2024-12-15 04:55:50 -0600

    changed tab indent to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,10 @@
 -- Add any additional options here
 --
 -- Tabs/Spacing
-vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- Always open CWD for file in Terminal
 vim.opt.autochdir = true
